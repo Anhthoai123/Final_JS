7a4e389885b279e477a247b558509dc208554ba7
Anhthoai123 <Leanhthoai2001hiie@gmail.com>
2023-04-08 13:49:24 +0700

    123

---
+++ digitic-website-master/src/components/ProductCard.js
@@ -0,0 +1,140 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link, useLocation } from "react-router-dom";
+import prodcompare from "../images/prodcompare.svg";
+import wish from "../images/wish.svg";
+import wishlist from "../images/wishlist.svg";
+import watch from "../images/watch.jpg";
+import watch2 from "../images/watch-1.avif";
+import addcart from "../images/add-cart.svg";
+import view from "../images/view.svg";
+const ProductCard = (props) => {
+  const { grid } = props;
+  console.log(grid);
+  let location = useLocation();
+
+  return (
+    <>
+      <div
+        className={` ${
+          location.pathname == "/product" ? `gr-${grid}` : "col-3"
+        } `}
+      >
+        <Link
+          to={`${
+            location.pathname == "/"
+              ? "/product/:id"
+              : location.pathname == "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
+          <div className="wishlist-icon position-absolute">
+            <button className="border-0 bg-transparent">
+              <img src={wish} alt="wishlist" />
+            </button>
+          </div>
+          <div className="product-image">
+            <img src={watch} className="img-fluid" alt="product image" />
+            <img src={watch2} className="img-fluid" alt="product image" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">
+            Kids watch bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+            Hello dear parents,
+            Children's smart watch is a great product to help protect and care for your child. Not only a beautiful watch, 
+            this product also integrates many smart features to help your child feel more secure when going to school or going out.
+            Get a smart watch for kids now to help protect and take care of your beloved children!
+            </p>
+            <p className="price">$100.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <button className="border-0 bg-transparent">
+                <img src={prodcompare} alt="compare" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={view} alt="view" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={addcart} alt="addcart" />
+              </button>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={` ${
+          location.pathname == "/product" ? `gr-${grid}` : "col-3"
+        } `}
+      >
+        <Link
+          to={`${
+            location.pathname == "/"
+              ? "/product/:id"
+              : location.pathname == "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
+          <div className="wishlist-icon position-absolute">
+            <button className="border-0 bg-transparent">
+              <img src={wish} alt="wishlist" />
+            </button>
+          </div>
+          <div className="product-image">
+            <img src={watch} className="img-fluid" alt="product image" />
+            <img src={watch2} className="img-fluid" alt="product image" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">
+            Kids watch bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+            Hello dear parents,
+            Children's smart watch is a great product to help protect and care for your child. Not only a beautiful watch, 
+            this product also integrates many smart features to help your child feel more secure when going to school or going out.
+            Get a smart watch for kids now to help protect and take care of your beloved children!
+            </p>
+            <p className="price">$100.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <button className="border-0 bg-transparent">
+                <img src={prodcompare} alt="compare" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={view} alt="view" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={addcart} alt="addcart" />
+              </button>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
